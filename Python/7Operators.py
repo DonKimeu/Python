072a64e13fa391c3d474f1e8c4791ed093405591
DonKimeu <57639381+DonKimeu@users.noreply.github.com>
2020-10-24 15:23:09 +0300

    Operators first part

---
+++ Python/7Operators.py
@@ -0,0 +1,55 @@
+"""
+Operators are used to perform operations on variables and values
+
+>>>>>>>>>>Operator groups in Python<<<<<<<<<<<<<<<
+Arithmetic Operators
+> Used with numerical values to performs common mathematical operations
+
+Assignment Operators
+> Are used to assign values to variables
+
+Comparison Operators
+> These operators are used to compare values
+
+Logical Operators
+> They are used to combine conditional statements
+
+Identity Operators
+> Used to compare objects , not if they are equall but if they are actually the same object with the same memory allocation
+
+Membership Operators
+> They are used to test if a sequence is present in an objects
+
+Bitwise Operators
+> They are used to compare binary numbers
+"""
+
+# >>>>>>>>>>>>>>>> Arithmetic Operators <<<<<<<<<<<<<<<<<<<
+
+a = 20
+b = 30
+''' (+) Addition operator used to add numerical values '''
+print(a+b)      # Output : 50
+
+''' (-) Subtraction operator used to Subract numerical values '''
+print(b-a)      # Output : 10
+
+''' (*) Multiplication operator used to Multiply numerical values '''
+print(a*b)      # Output : 600
+
+''' (/) Division operator used to divide numerical values '''
+print(b/a)      # Output : 1.5
+
+''' (%) Modulus operator used to get the remainder after dividing numerical values '''
+print(b%a)      # Output : 10
+
+''' (**) Exponentiation operator used to return the value of raising the first operand to the power of the second operand '''
+print(a**b)      # Output : 1073741824000000000000000000000000000000
+
+''' (//) Floor division operator used to return the floor value of both integer and floating point values '''
+print(b//a)      # Output : 1
+
+
+
+
+# >>>>>>>>>>>>>>>> Assignment Operators <<<<<<<<<<<<<<<<<<<
